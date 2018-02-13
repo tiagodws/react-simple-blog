49fcbebd89ef7cbd4a01d3ecc1ef811a4258b4c1
Tiago Dionesto Willrich da Silva <tiagodws@gmail.com>
2018-02-13 15:33:46 -0200

    project creation

---
+++ README.md
@@ -0,0 +1,2 @@
+# react-simple-blog
+Projeto de blog com formulário e rotas, feito durante o curso https://www.udemy.com/react-redux/learn/v4/overview
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+
+module.exports = () => {
+    const BUILD_FOLDER = "dist";
+    const BUNDLE_NAME = "bundle";
+
+    return {
+        context: path.join(__dirname, "src"),
+        entry: ["./index.js"],
+        output: {
+            path: path.join(__dirname, BUILD_FOLDER),
+            filename: `${BUNDLE_NAME}.js`,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: ["babel-loader"],
+                },
+            ],
+        },
+        resolve: {
+            extensions: [".js", ".jsx"],
+            modules: [path.join(__dirname, "node_modules")],
+        },
+        devtool: "source-map",
+        devServer: {
+            historyApiFallback: true,
+            contentBase: "./",
+            compress: true,
+            open: true,
+            stats: "minimal",
+            overlay: true,
+        },
+    };
+};
